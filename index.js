504a03dd93e2a70597e1500a8dcb534ee08cc218
Pedro Reis <itspdro@outlook.com>
2023-05-10 10:16:18 -0300

    test: unit section

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const headerIMG = document.querySelector('.header');
 const menuIcon = document.querySelector('.nav-icon');
 
 class headerSlider {
-    
+
     #currentImgIndex = 0;
     buttons;
 
-    constructor(link){
+    constructor(link) {
         this.link = link;
         this.createSlider();
     };
@@ -18,15 +18,15 @@ class headerSlider {
         this.autoSlider();
     };
 
-    createImg(){
+    createImg() {
         headerIMG.style.backgroundImage = `url(${this.link[this.#currentImgIndex]})`;
     };
 
-    createBtn(){
+    createBtn() {
         this.buttonContainer = document.createElement('div');
         this.buttonContainer.classList.add('btns-slider-header');
         headerIMG.appendChild(this.buttonContainer);
-        this.buttons = this.link.map((btn , index) =>{
+        this.buttons = this.link.map((btn, index) => {
             btn = document.createElement('button');
             btn.classList.add('btn-slider-header');
             btn.innerHTML = '<i class="fa-solid fa-circle-dot"></i>';
@@ -40,18 +40,18 @@ class headerSlider {
         });
     };
 
-    activeBtn(){
+    activeBtn() {
         this.buttons.forEach((btn, index) => {
             index === this.#currentImgIndex ? btn.classList.add('active-img') : btn.classList.remove('active-img');
         });
     };
 
-    autoSlider(){
+    autoSlider() {
         setInterval(() => {
             this.#currentImgIndex = (this.#currentImgIndex + 1) % this.link.length;
             this.createImg();
             this.activeBtn();
-        }, 3000);
+        }, 5000);
     }
 
 };
@@ -59,46 +59,37 @@ class headerSlider {
 const headerImagens = [
     new headerSlider([
         './assets/content-images/header1.jpg',
-        './assets/content-images/header2.jpg',
-        './assets/content-images/header3.jpg'
+        './assets/content-images/header2.jpg'
     ]),
 ];
 
 menuIcon.addEventListener('click', () => {
     document.querySelector('.navbar').classList.toggle('active-nav');
-})
-class mainDif {
-    constructor(icone, texto) {
-        this.icone = icone;
-        this.texto = texto;
-    }
+});
 
-    createDiv() {
-        const div = document.createElement('div');
-        div.classList.add('diferenciais-item');
-        div.innerHTML = `
-        <div class="diferenciais-icone">
-            <i class="fa-solid ${this.icone} fa-2xl"></i>
-        </div>
-        <div class="diferenciais-texto">
-            <h3>${this.texto}</h3>
-        </div>
-        `;
-        return div;
-    }
+// class unitImgs {
 
-    displayDiff() {
-        const main = document.querySelector('.main-diferenciais');
-        main.appendChild(this.createDiv());
-    }
-}
+//     #currentImgIndex = 0;
+//     buttons;
 
-const diferenciais = [
-    new mainDif('fa-volleyball', 'Esportes'),
-    new mainDif('fa-guitar', 'Músicas'),
-    new mainDif('fa-palette', 'Arte'),
-];
+//     constructor(url) {
+//         this.url = url;
+//         this.drawnIMG();
+//     }
 
-for (dif of diferenciais) {
-    dif.displayDiff();
-};
+//     drawnIMG() {
+//         this.url.map((img, index) => {
+//             this.#currentImgIndex = index;
+//             img = document.createElement('img');
+//             img.classList.add('unit-img');
+//             img.src = this.url[index];
+//             document.querySelector('.unit-imgs').appendChild(img);
+//         });
+//     }
+// }
+
+// let allIMG = new unitImgs([
+//     "https://placehold.co/600x400?text=Serviço+1&font=roboto",
+//     "https://placehold.co/600x400?text=Serviço+2&font=roboto",
+//     "https://placehold.co/600x400?text=Serviço+3&font=roboto",
+// ]);
