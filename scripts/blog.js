db59638e40f25936bea87d9098a199c950c0c29a
Pedro <pedluhi@outlook.com>
2023-05-16 11:44:06 -0300

    chore: updating blog method

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,16 +1,11 @@
-import { getPosts } from "../utils/misc";
+import { getPosts } from "../utils/misc.js";
 
 class BlogPosts {
+
     constructor() {
         this.posts = [];
-        this.container = document.querySelector('.main-posts');
-        this.template = document.querySelector('.post-template');
-        this.loadPosts();
-    }
-
-    async loadPosts() {
-        const response = await getPosts();
-        this.posts = response;
+        this.container = document.querySelector('.posts');
+        this.template = document.querySelector('.post');
         this.renderPosts();
     }
 
@@ -18,8 +13,21 @@ class BlogPosts {
         const fragment = document.createDocumentFragment();
         this.posts.forEach((post) => {
             const element = this.template.content.cloneNode(true);
-        })
+            element.querySelector('.post-title').innerHTML = post.title;
+            element.querySelector('.post-description').innerHTML = post.description;
+            element.querySelector('.post-date').innerHTML = post.date;
+            element.querySelector('.post-author').innerHTML = post.author;
+            element.querySelector('.post-url').innerHTML = post.url;
+
+        });
+        this.container.appendChild(fragment);
     }
 
-
 }
+
+const posts = await getPosts();
+new BlogPosts(posts);
+
+console.log(posts)
+
+
--- scripts/post.js
+++ scripts/post.js
@@ -1,7 +1,18 @@
-class PostPage {
-    constructor() {
-        this.posts = [];
-        this.container = document.createElement('.main-blog');
-        this.template = ''
-    }
+export function PostFactory() {
+    const createPost = (url, author, title, description, body) => {
+      return {
+        id: Math.random(),
+        author,
+        date: new Date(),
+        url,
+        title,
+        description,
+        body,
+      };
+    };
+  
+    return {
+      createPost,
+    };
 }
+  
