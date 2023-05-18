bf1ad92eb6d472ef0821499c3a44d065c4155a8d
Pedro <pedluhi@outlook.com>
2023-05-18 11:49:59 -0300

    refactor: components add

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,13 +1,49 @@
-const menuIcon = document.querySelector('.nav-icon');
-
-menuIcon.addEventListener('click', () => {
-    document.querySelector('.navbar').classList.toggle('active-nav');
-    const icon = menuIcon.querySelector('i');
-    if (icon.classList.contains('fa-bars')) {
-        icon.classList.remove('fa-bars');
-        icon.classList.add('fa-times');
-    } else {
-        icon.classList.remove('fa-times');
-        icon.classList.add('fa-bars');
+const navContent = document.querySelector('.navbar');
+class navFactory {
+    constructor(){
+        this.createNav();
     }
-});
+    
+    createNav() {
+        navContent.innerHTML = `
+            <a href="../index.html">
+                <img src="../assets/Horizon-logo.png" alt="Horizon Logo" class="logo">
+            </a>
+            <div class="nav-icon"><i class="fa-solid fa-bars"></i></div>
+            
+            <div class="nav-links">
+                <ul>
+                    <li><a class="nav-link" href="@/index.html"> Inicio</li></a>
+                    <li><a class="nav-link" href="@/index.html#diferenciais">Diferenciais</li></a>
+                    <li><a class="nav-link" href="@/index.html#unidades">Unidades</li></a>
+                    <li><a class="nav-link" href="@/index.html#comentarios">Comentários</li></a>
+                    <li><a class="nav-link" href="#contato">Contato</li></a>
+                    <li><a class="nav-link" href="https://itspedro.github.io/horizon/pages/help.html">Dúvidas</li></a>
+                </ul>
+                <span class="social-links-nav">
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                </span>
+            </div>
+            
+            `;
+            this.menuIcon();
+    }
+
+    menuIcon() {
+        const menuIcon = document.querySelector('.nav-icon');
+        menuIcon.addEventListener('click', () => {
+            document.querySelector('.navbar').classList.toggle('active-nav');
+            const icon = menuIcon.querySelector('i');
+            if (icon.classList.contains('fa-bars')) {
+                icon.classList.remove('fa-bars');
+                icon.classList.add('fa-times');
+            } else {
+                icon.classList.remove('fa-times');
+                icon.classList.add('fa-bars');
+            }
+        });
+    }
+
+}
+new navFactory();
--- scripts/post.js
+++ scripts/post.js
@@ -8,30 +8,33 @@ async function fetchPost() {
     const post = await getPostById(postId);
     console.log(post);
     const postContentDiv = document.querySelector('.post-main');
+
     postContentDiv.innerHTML = `
-    <div class="main-blog">
-    <div class="main-img">
-        <img src="${post.url}" alt="post image">
-    </div>
-    <div class="blog-header">
-        <div class="blog-header-content">
-            <h1>${post.title}</h1>
-            <div class="blog-metadatas">
-                <span>${post.author}</span>
-                <p>${post.date}</p>
-            </div>
-            <div class="blog-description">
-                <p>${post.description}</p>
+      <div class="main-blog">
+        <div class="main-img">
+          <img src="${post.url}" alt="post image">
+        </div>
+        <div class="blog-header">
+            <div class="blog-header-content">
+                <h1>${post.title}</h1>
+                <div class="blog-metadatas">
+                  <span>${post.author}</span>
+                  <p>${post.date}</p>
+                </div>
+                <div class="blog-description">
+                  <p>${post.description}</p>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="blog-content">
-       <p>${post.body}</p>
-    </div>
-</div>`;
+        <div class="blog-content">
+          <p>${post.body}</p>
+        </div>
+      </div>
+    `;
+
   } catch (error) {
     console.error('Error fetching post:', error);
   }
 }
-// Fetch the post details on page load
+
 fetchPost();
